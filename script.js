9b01df25798674eee1a2390ad5252c643bd39531
davideblaffard <infodavide98@gmail.com>
2025-07-08 01:26:48 +0200

    add: added some changes

--- script.js
+++ script.js
@@ -19,3 +19,15 @@ const toggleBtn = document.getElementById("theme-toggle");
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 });
+
+
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("visible");
+    navbar.classList.remove("hidden");
+  } else {
+    navbar.classList.add("hidden");
+    navbar.classList.remove("visible");
+  }
+});
